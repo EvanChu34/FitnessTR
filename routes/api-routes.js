b457d1827297376e35c17cf731e477357eccd465
Evan Chu <echu34@gmail.com>
2021-03-12 20:50:53 -0500

    more work on the routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,3 +18,19 @@ router.get("/api/workouts", (req, res) =>{
     });
 
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
+
